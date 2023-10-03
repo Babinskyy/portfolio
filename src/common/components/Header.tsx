4395208a36b9876f9c5648bde2e94f6ed2b14018
Babinskyy <janbbnsk@protonmail.com>
2023-10-03 14:23:44 +0200

    update

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -22,7 +22,7 @@ const Header = (props: HeaderProps): JSX.Element => {
           to="/"
           style={{
             textDecoration: "none",
-            color: `${props.darkTheme ? "#dfdfdf" : "#2b2b2b"}`,
+            color: `${props.darkTheme ? "#fff" : "#2b2b2b"}`,
           }}
         >
           <div className="logo-container">
@@ -30,27 +30,16 @@ const Header = (props: HeaderProps): JSX.Element => {
             <div className="logo">Yan Software</div>
           </div>
         </Link>
-        <div className="icons-toggle-container">
-          <Link to="https://github.com/Babinskyy" target="_blank">
-            <img src={githublogo} alt="" />
-          </Link>
-          <Link
-            to="https://www.linkedin.com/in/jan-babinski-880469229/"
-            target="_blank"
-          >
-            <img src={linkedin} alt="icon" />
-          </Link>
 
-          <div className="toggle-switch">
-            <label className={`${props.darkTheme ? "dark" : ""}`}>
-              <input
-                type="checkbox"
-                onChange={handleModeChange}
-                checked={!props.darkTheme}
-              />
-              <span className="slider-toggle"></span>
-            </label>
-          </div>
+        <div className="toggle-switch">
+          <label className={`${props.darkTheme ? "dark" : ""}`}>
+            <input
+              type="checkbox"
+              onChange={handleModeChange}
+              checked={!props.darkTheme}
+            />
+            <span className="slider-toggle"></span>
+          </label>
         </div>
       </div>
     </header>
--- src/pages/home/components/Greetings.tsx
+++ src/pages/home/components/Greetings.tsx
@@ -1,17 +1,27 @@
 import pic from "../../../common/images/others/papugapic2.png";
 type GreetingsProps = {
-    darkTheme: boolean;
-}
+  darkTheme: boolean;
+};
 
-const Greetings = (props:GreetingsProps):JSX.Element => {
-    return ( <div className="greetings-container">
-    <div className={`image-container ${props.darkTheme ? "dark" : ""}`}>
-      <img src={pic} alt="" />
+const Greetings = (props: GreetingsProps): JSX.Element => {
+  return (
+    <div className="greetings-container">
+      <div className={`image-container ${props.darkTheme ? "dark" : ""}`}>
+        <img src={pic} alt="" />
+      </div>
+      <div className="introduction">
+        <h1 className="greetings">Hello, I'm Jan.</h1>
+        <p className="description">
+          As a full-stack JavaScript developer, I'm deeply passionate about
+          programming and have a desire to learn new technologies.
+          <br />
+          <br />
+          When I'm not immersed in code, you'll find me enjoying my love for
+          chess or absorbed in the fascinating world of books.
+        </p>
+      </div>
     </div>
-    <h1 className="greetings">Hello, I'm Jan.</h1>
-    
-    {/* <div className="filler"></div> */}
-  </div>)
-}
+  );
+};
 
 export default Greetings;
